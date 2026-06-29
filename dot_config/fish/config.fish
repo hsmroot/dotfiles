d95234eea1d2d866686a238b8f1e303ef9240620
hsmroot <your_email@example.com>
2026-06-29 15:02:51 +1200

    auto-backup: 2026-06-29

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -82,8 +82,8 @@ end
 function copy
     set count (count $argv | tr -d \n)
     if test "$count" = 2; and test -d "$argv[1]"
-	set from (echo $argv[1] | string trim --right --chars=/)
-	set to (echo $argv[2])
+        set from (echo $argv[1] | string trim --right --chars=/)
+        set to (echo $argv[2])
         command cp -r $from $to
     else
         command cp $argv
@@ -100,7 +100,28 @@ function cleanup
     end
 end
 
+# Silently export system manifests and sync Dotfiles to GitHub
+function dotsync
+    echo "📦 Exporting package and Flatpak lists..."
+    pacman -Qqe > ~/.config/pkglist.txt
+    flatpak list --app --columns=application > ~/.config/flatpaklist.txt
+    
+    echo "🔍 Pulling latest system configs into Chezmoi..."
+    chezmoi re-add
+    
+    echo "☁️ Pushing to GitHub..."
+    chezmoi cd
+    git add .
+    git commit -m "auto-sync: "(date '+%Y-%m-%d %H:%M')
+    git push origin main
+    cd -
+    echo "✅ Dotfiles successfully synced to cloud!"
+end
+
 ## Useful aliases
+# Automation Shortcuts
+alias sync 'dotsync'
+
 # Replace ls with eza
 alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
 alias lsz 'eza -al --color=always --total-size --group-directories-first --icons' # include file size
@@ -135,32 +156,4 @@ alias psmem 'ps auxf | sort -nr -k 4'
 alias psmem10 'ps auxf | sort -nr -k 4 | head -10'
 alias rmpkg 'sudo pacman -Rdd'
 alias tarnow 'tar -acf '
-alias untar 'tar -zxvf '
-alias upd '/usr/bin/garuda-update'
-alias vdir 'vdir --color=auto'
-alias wget 'wget -c '
-
-# Get fastest mirrors
-alias mirror 'sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist'
-alias mirrora 'sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist'
-alias mirrord 'sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist'
-alias mirrors 'sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist'
-
-# Help people new to Arch
-alias apt 'man pacman'
-alias apt-get 'man pacman'
-alias please 'sudo'
-alias tb 'nc termbin.com 9999'
-alias helpme 'echo "To print basic information about a command use tldr <command>"'
-alias pacdiff 'sudo -H DIFFPROG=meld pacdiff'
-
-# Get the error messages from journalctl
-alias jctl 'journalctl -p 3 -xb'
-
-# Recent installed packages
-alias rip 'expac --timefmt="%Y-%m-%d %T" "%l\t%n %v" | sort | tail -200 | nl'
-
-## Run fastfetch if session is interactive
-if status --is-interactive && type -q fastfetch
-   fastfetch --config neofetch.jsonc
-end
+alias untar 'tar -zxv'
